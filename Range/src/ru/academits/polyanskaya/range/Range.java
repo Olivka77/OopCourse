a23b29cb4a1ed583b27b5cf41fdc3272009d0be7
Olivka77 <alinkin07@inbox.ru>
2023-01-20 23:43:23 +0700

    Add getIntersection, getUnion, getDifference methods

--- Range/src/ru/academits/polyanskaya/range/Range.java
+++ Range/src/ru/academits/polyanskaya/range/Range.java
@@ -32,4 +32,78 @@ public class Range {
     public boolean isInside(double number) {
         return number >= from && number <= to;
     }
+
+    public Range getIntersection(Range range) {
+        if (range.from == from && range.to == to) {
+            return range;
+        }
+
+        if (range.from < to && range.to > from || range.from > to && range.to < from) {
+            return new Range(Math.max(range.from, from), Math.min(range.to, to));
+        }
+
+        return null;
+    }
+
+    public Range[] getUnion(Range range) {
+        if (range.from == from && range.to == to) {
+            Range[] rangeArray = new Range[1];
+
+            rangeArray[0] = range;
+
+            return rangeArray;
+        }
+
+        if (range.from <= to && range.to >= from || range.from >= to && range.to <= from) {
+            Range[] rangeArray = new Range[1];
+
+            rangeArray[0] = new Range(Math.min(range.from, from), Math.max(range.to, to));
+
+            return rangeArray;
+        }
+
+        Range[] rangesArray = new Range[2];
+
+        rangesArray[0] = new Range(from, to);
+        rangesArray[1] = new Range(range.from, range.to);
+
+        return rangesArray;
+    }
+
+    public Range[] getDifference(Range range) {
+        if (range.from == from && range.to == to || from >= range.from && to <= range.to) {
+            return null;
+        }
+
+        if (from < range.to && to < range.from || from > range.to && to > range.from) {
+            Range[] rangeArray = new Range[1];
+
+            rangeArray[0] = new Range(from, to);
+
+            return rangeArray;
+        }
+
+        if (from < range.from && to > range.to) {
+            Range[] rangesArray = new Range[2];
+
+            rangesArray[0] = new Range(from, range.from);
+            rangesArray[1] = new Range(range.to, to);
+
+            return rangesArray;
+        }
+
+        if (range.from <= from) {
+            Range[] rangeArray = new Range[1];
+
+            rangeArray[0] = new Range(range.to, to);
+
+            return rangeArray;
+        }
+
+        Range[] rangeArray = new Range[1];
+
+        rangeArray[0] = new Range(from, range.from);
+
+        return rangeArray;
+    }
 }
--- Range/src/ru/academits/polyanskaya/range_main/RangeMain.java
+++ Range/src/ru/academits/polyanskaya/range_main/RangeMain.java
@@ -6,15 +6,16 @@ import java.util.Scanner;
 
 public class RangeMain {
     public static void main(String[] args) {
-        Range range1 = new Range(-1, 8);
-        Range range2 = new Range(15, 25.5);
+        Range range1 = new Range(3, 12);
 
-        System.out.println("Длина диапазона от " + range1.getFrom() + " до " + range1.getTo() + ": " + range1.getLength());
+        System.out.printf("Длина диапазона от %.2f до %.2f: %.2f.%n", range1.getFrom(), range1.getTo(), range1.getLength());
 
-        range2.setFrom(30);
-        range2.setTo(50.1);
+        Range range2 = new Range(4, 8);
 
-        System.out.println("Длина диапазона от " + range2.getFrom() + " до " + range2.getTo() + ": " + range2.getLength());
+        range2.setFrom(13);
+        range2.setTo(15);
+
+        System.out.printf("Длина диапазона от %.2f до %.2f: %.2f.%n%n", range2.getFrom(), range2.getTo(), range2.getLength());
 
         Scanner scanner = new Scanner(System.in);
 
@@ -22,9 +23,42 @@ public class RangeMain {
         double number = scanner.nextDouble();
 
         if (range1.isInside(number)) {
-            System.out.println("Данное число принадлежит диапазону от " + range1.getFrom() + " до " + range1.getTo());
+            System.out.printf("Данное число принадлежит диапазону от %.2f до %.2f.%n%n", range1.getFrom(), range1.getTo());
         } else {
-            System.out.println("Данное число не принадлежит диапазону от " + range1.getFrom() + " до " + range1.getTo());
+            System.out.printf("Данное число не принадлежит диапазону от %.2f до %.2f.%n%n", range1.getFrom(), range1.getTo());
+        }
+
+        Range range3 = range1.getIntersection(range2);
+
+        if (range3 == null) {
+            System.out.printf("Диапазоны от %.2f до %.2f и от %.2f до %.2f не пересекаются.%n%n",
+                    range1.getFrom(), range1.getTo(), range2.getFrom(), range2.getTo());
+        } else {
+            System.out.printf("Диапазоны от %.2f до %.2f и от %.2f до %.2f пересекаются на интервале от %.2f до %.2f.%n%n",
+                    range1.getFrom(), range1.getTo(), range2.getFrom(), range2.getTo(), range3.getFrom(), range3.getTo());
+        }
+
+        Range[] range4 = range1.getUnion(range2);
+
+        System.out.printf("Объединение диапазонов от %.2f до %.2f и от %.2f до %.2f: от %.2f до %.2f", range1.getFrom(),
+                range1.getTo(), range2.getFrom(), range2.getTo(), range4[0].getFrom(), range4[0].getTo());
+
+        if (range4.length > 1) {
+            System.out.printf(" и от %.2f до %.2f", range4[1].getFrom(), range4[1].getTo());
+        }
+
+        Range[] range5 = range1.getDifference(range2);
+
+        if (range5 == null) {
+            System.out.printf(".%n%nРазность диапазонов от %.2f до %.2f и от %.2f до %.2f - пустое множество.%n%n",
+                    range1.getFrom(), range1.getTo(), range2.getFrom(), range2.getTo());
+        } else {
+            System.out.printf(".%n%nРазность диапазонов от %.2f до %.2f и от %.2f до %.2f: от %.2f до %.2f", range1.getFrom(),
+                    range1.getTo(), range2.getFrom(), range2.getTo(), range5[0].getFrom(), range5[0].getTo());
+
+            if (range5.length > 1) {
+                System.out.printf(" и от %.2f до %.2f.%n", range5[1].getFrom(), range5[1].getTo());
+            }
         }
     }
 }

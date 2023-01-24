2e2ca5dd856b8a0efcaf0d3a9c821637cfaaa679
Olivka77 <alinkin07@inbox.ru>
2023-01-24 23:13:37 +0700

    Initial commit

---
+++ Shapes/src/ru/academits/polyanskaya/shapes_circle/Circle.java
@@ -0,0 +1,25 @@
+package ru.academits.polyanskaya.shapes_circle;
+
+import ru.academits.polyanskaya.shapes_interface.ShapesInterface;
+
+public class Circle implements ShapesInterface {
+    @Override
+    public double getWidth() {
+        return 0;
+    }
+
+    @Override
+    public double getHeight() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+}
---
+++ Shapes/src/ru/academits/polyanskaya/shapes_main/ShapesMain.java
@@ -0,0 +1,7 @@
+package ru.academits.polyanskaya.shapes_main;
+
+public class ShapesMain {
+    public static void main(String[] args) {
+
+    }
+}

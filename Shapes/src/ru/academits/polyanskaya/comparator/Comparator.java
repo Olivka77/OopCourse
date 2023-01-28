3fae00a8b71a6986118eb2ebee993810e7f810f5
Olivka77 <alinkin07@inbox.ru>
2023-01-28 19:02:07 +0700

    Renamed Comparator

--- Shapes/src/ru/academits/polyanskaya/comparator/Comparator.java
+++ Shapes/src/ru/academits/polyanskaya/comparator/Comparator.java
@@ -3,22 +3,21 @@ package ru.academits.polyanskaya.comparator;
 import ru.academits.polyanskaya.shape_interface.ShapeInterface;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
-public class AreaPerimeterComparator implements Comparator<ShapeInterface> {
+public class Comparator implements java.util.Comparator<ShapeInterface> {
     @Override
     public int compare(ShapeInterface shape1, ShapeInterface shape2) {
         return (int) (shape1.getArea() - shape2.getArea());
     }
 
     public static ShapeInterface getShapeMaxArea(ShapeInterface[] shapesArray) {
-        Arrays.sort(shapesArray, new AreaPerimeterComparator());
+        Arrays.sort(shapesArray, new Comparator());
 
         return shapesArray[shapesArray.length - 1];
     }
 
     public static ShapeInterface getShapePreviousMaxPerimeter(ShapeInterface[] shapesArray) {
-        Arrays.sort(shapesArray, new AreaPerimeterComparator());
+        Arrays.sort(shapesArray, new Comparator());
 
         return shapesArray[shapesArray.length - 2];
     }
--- Shapes/src/ru/academits/polyanskaya/shape_main/ShapeMain.java
+++ Shapes/src/ru/academits/polyanskaya/shape_main/ShapeMain.java
@@ -1,7 +1,7 @@
 package ru.academits.polyanskaya.shape_main;
 
 import ru.academits.polyanskaya.circle.Circle;
-import ru.academits.polyanskaya.comparator.AreaPerimeterComparator;
+import ru.academits.polyanskaya.comparator.Comparator;
 import ru.academits.polyanskaya.rectangle.Rectangle;
 import ru.academits.polyanskaya.shape_interface.ShapeInterface;
 import ru.academits.polyanskaya.square.Square;
@@ -18,8 +18,8 @@ public class ShapeMain {
                 new Triangle(-1, -1, -3, -2, -2, -4),
                 new Triangle(1, 2, 3.5, 5, 3.5, 2)};
 
-        System.out.println("Фигура с максимальной площадью - " + AreaPerimeterComparator.getShapeMaxArea(shapesArray));
-        System.out.println("Фигура со вторым по величине периметром - " + AreaPerimeterComparator.getShapePreviousMaxPerimeter(shapesArray));
+        System.out.println("Фигура с максимальной площадью - " + Comparator.getShapeMaxArea(shapesArray));
+        System.out.println("Фигура со вторым по величине периметром - " + Comparator.getShapePreviousMaxPerimeter(shapesArray));
         System.out.println("Все фигуры: " + System.lineSeparator() + Arrays.toString(shapesArray));
         System.out.println("Проверка переопределения equals: " + shapesArray[6].equals(shapesArray[7]));
     }

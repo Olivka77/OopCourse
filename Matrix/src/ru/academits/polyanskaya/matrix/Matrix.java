7b1560cf16d95fa181de2a00a9579aa61c0b91bd
Olivka77 <alinkin07@inbox.ru>
2023-02-15 22:08:33 +0700

    In process

--- Matrix/src/ru/academits/polyanskaya/matrix/Matrix.java
+++ Matrix/src/ru/academits/polyanskaya/matrix/Matrix.java
@@ -2,15 +2,81 @@ package ru.academits.polyanskaya.matrix;
 
 import ru.academits.polyanskaya.vector.Vector;
 
-public class Matrix extends Vector {
-    private int n;
-    private int m;
-    private double[][] matrix;
+import java.util.Arrays;
 
-    public Matrix(int dimension, int n, int m) {
-        super(dimension);
-        this.n = n;
-        this.m = m;
-        this.matrix = new double[n][m];
+public class Matrix {
+    private final Vector[] matrixRows;
+
+    public Matrix(int n, int m) {
+        int maxSize = Math.max(n, m);
+        matrixRows = new Vector[maxSize];
+
+        for (int i = 0; i < maxSize; i++) {
+            matrixRows[i] = new Vector(maxSize);
+        }
+    }
+
+    public Matrix(Matrix copy) {
+        this(copy.matrixRows);
+    }
+
+    public Matrix(double[][] array) {
+        matrixRows = new Vector[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            matrixRows[i] = new Vector(Arrays.copyOf(array[i], array.length));
+        }
+    }
+
+    public Matrix(Vector[] vectors) {
+        matrixRows = new Vector[vectors.length];
+
+        for (int i = 0; i < vectors.length; i++) {
+            int vectorSize = vectors[i].getSize();
+
+            double[] components = new double[vectorSize];
+
+            for (int j = 0; j < vectorSize; j++) {
+                components[j] = vectors[i].getComponent(j);
+            }
+
+            matrixRows[i] = new Vector(vectors.length, components);
+        }
+    }
+
+    public int getSize() {
+        int matrixSize = matrixRows.length;
+
+        for (Vector matrixRow : matrixRows) {
+            if (matrixRow.getSize() != matrixSize) {
+                throw new IllegalArgumentException("Матрица " + matrixSize + " x " + matrixRow.getSize() +
+                        " не является квадратной");
+            }
+        }
+
+        return matrixSize;
+    }
+
+    public Vector getVector(int index) {
+
+    }
+
+    public void setVector(int index) {
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("{");
+
+        for (Vector vector : matrixRows) {
+            stringBuilder.append(vector.toString()).append(", ");
+        }
+
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1).deleteCharAt(stringBuilder.length() - 1).append("}");
+
+        return stringBuilder.toString();
     }
 }
--- Matrix/src/ru/academits/polyanskaya/matrix_main/MatrixMain.java
+++ Matrix/src/ru/academits/polyanskaya/matrix_main/MatrixMain.java
@@ -1,7 +1,38 @@
 package ru.academits.polyanskaya.matrix_main;
 
+import ru.academits.polyanskaya.matrix.Matrix;
+import ru.academits.polyanskaya.vector.Vector;
+
 public class MatrixMain {
     public static void main(String[] args) {
+        double[][] array1 = {
+                {0, 1, 2, 3, 1},
+                {2, 1, 3, 1, 1},
+                {4, 3, 7, 1, 1},
+                {9, 7, 4}
+        };
 
+        System.out.println(array1.length);
+        System.out.println(array1[0].length);
+        System.out.println(array1[3].length);
+
+        Matrix matrix1 = new Matrix(5, 4);
+        Matrix matrix2 = new Matrix(matrix1);
+        Matrix matrix3 = new Matrix(array1);
+        matrix1 = matrix3;
+
+        Vector[] vector1 = {
+                new Vector(array1[3]),
+                new Vector(array1[1])
+        };
+
+        Matrix matrix4 = new Matrix(vector1);
+
+        System.out.println(matrix1);
+        System.out.println(matrix2);
+        System.out.println(matrix3);
+        System.out.println(matrix2);
+        System.out.println(matrix4);
+        System.out.println(matrix4.getSize());
     }
 }

c121392e32451c6e45c2e219138bd5b4a6b50388
Olivka77 <alinkin07@inbox.ru>
2023-02-17 19:57:35 +0700

    Added import Comparator

--- Shapes/src/ru/academits/polyanskaya/comparators/ShapesByPerimeterComparator.java
+++ Shapes/src/ru/academits/polyanskaya/comparators/ShapesByPerimeterComparator.java
@@ -2,7 +2,9 @@ package ru.academits.polyanskaya.comparators;
 
 import ru.academits.polyanskaya.shapes.Shape;
 
-public class ShapesByPerimeterComparator implements java.util.Comparator<Shape> {
+import java.util.Comparator;
+
+public class ShapesByPerimeterComparator implements Comparator<Shape> {
     @Override
     public int compare(Shape shape1, Shape shape2) {
         return Double.compare(shape1.getPerimeter(), shape2.getPerimeter());

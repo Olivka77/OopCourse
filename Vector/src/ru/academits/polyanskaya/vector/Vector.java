7fba0554c66221a034b881b62ea9c557339ae5a9
Olivka77 <alinkin07@inbox.ru>
2023-03-05 11:55:11 +0700

    Edited

--- Vector/src/ru/academits/polyanskaya/vector/Vector.java
+++ Vector/src/ru/academits/polyanskaya/vector/Vector.java
@@ -57,7 +57,7 @@ public class Vector {
             components = Arrays.copyOf(components, vector.components.length);
         }
 
-        for (int i = 0; i < components.length; i++) {
+        for (int i = 0; i < vector.components.length; i++) {
             components[i] += vector.components[i];
         }
 
